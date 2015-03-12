324c4108465f0b627b792275c534f7ceda5abf93
Fabio Fazio <fabio.fazio@zenatek.eu>
2015-03-12 12:28:33 +0100

    #21 ridotto a 25 lunghezza min della descrizione welcome con dimensione standard compatibilie con smartphone predisposte classi per multidimensione in base al client px size Rinominate le intestazioni dei box esteso nuovo utente per test con username stefano

--- data/sql/install.sql
+++ data/sql/install.sql
@@ -2,8 +2,9 @@ INSERT INTO `hd0`.`Service` (`company`, `type`, `location`, `username`, `passwor
 	VALUES ('Zenatek S.P.A.', 'OTRS', 'http://ztac.zenatek.eu/otrs/nph-genericinterface.pl/Webservice/GenericTicketConnector', 'fabio.fazio', 'aaAA11!!', 'http://www.otrs.org/TicketConnector/');
 
 INSERT INTO `hd0`.`Queue` (`id`, `service_id`, `order`, `name`, `code`)
-	VALUES (NULL, '1', '0', 'ZTAC Incoming Queue', '5'),
+	VALUES	(NULL, '1', '0', 'ZTAC Incoming Queue', '5'),
 		(NULL, '1', '1', 'Junk', '3');
 
 INSERT INTO `hd0`.`User` (`id`, `name`, `email`, `password`, `username`)
-	VALUES (NULL, 'Fabio', 'fmfazio@gmail.com', 123, 'fabio');
+	VALUES	(NULL, 'Fabio', 'fmfazio@gmail.com', 123, 'fabio'),
+		(NULL, NULL, 'stefano', 123, 'stefano');
