35e1b6732d6a84b35b0852d1f4baa11522f23b0b
Fabio Fazio <fabio.fazio@zenatek.eu>
2015-03-16 21:09:31 +0100

    corretto script di install e segnalazioni inviate rinominate in aperte dove mancava

--- data/sql/install.sql
+++ data/sql/install.sql
@@ -1,10 +1,11 @@
 INSERT INTO `hd0`.`Service` (`company`, `type`, `location`, `username`, `password`, `namespace`) 
-	VALUES ('Zenatek S.P.A.', 'OTRS', 'http://ztac.zenatek.eu/otrs/nph-genericinterface.pl/Webservice/GenericTicketConnector', 'fabio.fazio', 'aaAA11!!', 'http://www.otrs.org/TicketConnector/');
+	VALUES ('Zenatek S.P.A.', 'OTRS', 'http://localhost/otrs/nph-genericinterface.pl/Webservice/GenericTicketConnector', 'hd0', 'hd0', 'http://www.otrs.org/TicketConnector/');
 
 INSERT INTO `hd0`.`Queue` (`id`, `service_id`, `order`, `name`, `code`)
-	VALUES	(NULL, '1', '0', 'ZTAC Incoming Queue', '5'),
-		(NULL, '1', '1', 'Junk', '3');
+	VALUES	(NULL, '1', '0', 'EasyPOP', '5'),
+		(NULL, '1', '1', 'ContPV', '3');
 
 INSERT INTO `hd0`.`User` (`id`, `name`, `email`, `password`, `username`)
 	VALUES	(NULL, 'Fabio', 'fmfazio@gmail.com', 123, 'fabio'),
-		(NULL, NULL, 'stefano', 123, 'stefano');
+		(NULL, NULL, 'stefano', 123, 'stefano'),
+		(NULL, 'Mario', 'fabio.fazio@zenatek.it', 123, 'mario');
