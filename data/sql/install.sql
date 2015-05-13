f8345a1551db39fb3928391cc0136f5cc59fa633
Fabio Fazio <fmfazio@gmail.com>
2015-05-13 19:20:07 +0200

    Install updated to populate basic Filters

--- data/sql/install.sql
+++ data/sql/install.sql
@@ -1,6 +1,84 @@
 INSERT INTO `hd0`.`Service` (`company`, `type`, `location`, `username`, `password`, `namespace`) 
 	VALUES ('Zenatek S.P.A.', 'OTRS', 'http://localhost/otrs/nph-genericinterface.pl/Webservice/GenericTicketConnector', 'hd0', 'hd0', 'http://www.otrs.org/TicketConnector/');
+	
 
+INSERT INTO `hd0`.`Filter` (`id`, `code`, `responce`, `question`, `askedBy_id`)
+	VALUES	(NULL, 'q05ar', NULL, 'Da quale dispositivo hai acceduto al servizio?',
+			NULL);
+
+INSERT INTO `hd0`.`Filter` (`id`, `code`, `responce`, `question`, `askedBy_id`)
+	SELECT NULL, 'q05ar1',
+		'PC', 'Che browser hai utilizzato per accedere al servizio?',
+		id FROM `hd0`.`Filter` WHERE code = 'q05ar';
+INSERT INTO `hd0`.`Filter` (`id`, `code`, `responce`, `question`, `askedBy_id`)
+	SELECT NULL, 'q05ar2',
+		'Tablet', NULL,
+		id FROM `hd0`.`Filter` WHERE code = 'q05ar';
+INSERT INTO `hd0`.`Filter` (`id`, `code`, `responce`, `question`, `askedBy_id`)
+	SELECT NULL, 'q05ar3',
+		'Smartphone',
+		'Che modello di smarphone hai utilizzato per accedere al servizio?',
+		id FROM `hd0`.`Filter` WHERE code = 'q05ar';
+INSERT INTO `hd0`.`Filter` (`id`, `code`, `responce`, `question`, `askedBy_id`)
+	SELECT NULL, 'q05ar4',
+		'Altro',
+		'In che momento si è riscontrato il problema?',
+		id FROM `hd0`.`Filter` WHERE code = 'q05ar';
+		
+		
+INSERT INTO `hd0`.`Filter` (`id`, `code`, `responce`, `question`, `askedBy_id`)
+	SELECT NULL, 'q05a1r1',
+		'Internet Explorer', NULL,
+		id FROM `hd0`.`Filter` WHERE code = 'q05ar1';
+INSERT INTO `hd0`.`Filter` (`id`, `code`, `responce`, `question`, `askedBy_id`)
+	SELECT NULL, 'q05a1r2',
+		'Firefox', NULL,
+		id FROM `hd0`.`Filter` WHERE code = 'q05ar1';
+INSERT INTO `hd0`.`Filter` (`id`, `code`, `responce`, `question`, `askedBy_id`)
+	SELECT NULL, 'q05a1r3',
+		'Chrome', NULL,
+		id FROM `hd0`.`Filter` WHERE code = 'q05ar1';
+INSERT INTO `hd0`.`Filter` (`id`, `code`, `responce`, `question`, `askedBy_id`)
+	SELECT NULL, 'q05a1r4',
+		'Altro', NULL,
+		id FROM `hd0`.`Filter` WHERE code = 'q05ar1';
+		
+		
+INSERT INTO `hd0`.`Filter` (`id`, `code`, `responce`, `question`, `askedBy_id`)
+	SELECT NULL, 'q05a3r1',
+		'Android', NULL,
+		id FROM `hd0`.`Filter` WHERE code = 'q05ar3';
+INSERT INTO `hd0`.`Filter` (`id`, `code`, `responce`, `question`, `askedBy_id`)
+	SELECT NULL, 'q05a3r2',
+		'IPhone', NULL,
+		id FROM `hd0`.`Filter` WHERE code = 'q05ar3';
+INSERT INTO `hd0`.`Filter` (`id`, `code`, `responce`, `question`, `askedBy_id`)
+	SELECT NULL, 'q05a3r3',
+		'Windows Mobile', NULL,
+		id FROM `hd0`.`Filter` WHERE code = 'q05ar3';
+INSERT INTO `hd0`.`Filter` (`id`, `code`, `responce`, `question`, `askedBy_id`)
+	SELECT NULL, 'q05a3r4',
+		'Altro', NULL,
+		id FROM `hd0`.`Filter` WHERE code = 'q05ar3';
+
+INSERT INTO `hd0`.`Filter` (`id`, `code`, `responce`, `question`, `askedBy_id`)
+	SELECT NULL, 'q05a4r1',
+		'Accesso al servizio', NULL,
+		id FROM `hd0`.`Filter` WHERE code = 'q05ar4';
+INSERT INTO `hd0`.`Filter` (`id`, `code`, `responce`, `question`, `askedBy_id`)
+	SELECT NULL, 'q05a4r2',
+		'Gestione delle utenze', NULL,
+		id FROM `hd0`.`Filter` WHERE code = 'q05ar4';
+INSERT INTO `hd0`.`Filter` (`id`, `code`, `responce`, `question`, `askedBy_id`)
+	SELECT NULL, 'q05a4r3',
+		'Finalizzazione di una stampa', NULL,
+		id FROM `hd0`.`Filter` WHERE code = 'q05ar4';
+INSERT INTO `hd0`.`Filter` (`id`, `code`, `responce`, `question`, `askedBy_id`)
+	SELECT NULL, 'q05a4r4',
+		'Altro', NULL,
+		id FROM `hd0`.`Filter` WHERE code = 'q05ar4';
+		
+		
 INSERT INTO `hd0`.`Queue` (`id`, `service_id`, `order`, `name`, `code`)
 	VALUES	(NULL, '1', '0', 'EasyPOP', '5'),
 		(NULL, '1', '1', 'ContPV', '3');
