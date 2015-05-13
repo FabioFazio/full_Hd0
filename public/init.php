146b54c7dfadd0b60f93f506b18250418db21250
Fabio Fazio <fmfazio@gmail.com>
2015-05-13 19:21:41 +0200

    init script to test, reset, populate database

--- public/init.php
+++ public/init.php
@@ -1,8 +1,11 @@
 <?php
+
 //$actions[] = 'cd ../vendor/bin/; ls -tharl';
+
 $actions[] = '../doctrine-module orm:validate-schema';
-//$actions[] = '../vendor/doctrine/doctrine-module/bin/doctrine-module orm:schema-tool:create';
-//$actions[] = '/usr/bin/mysql -uhd0 -p hd0 < ../data/sql/install.sql';
+//$actions[] = '/usr/bin/mysql -uhd0 -phd0 -e "drop database hd0; create database hd0;"';
+//$actions[] = '../doctrine-module orm:schema-tool:create';
+//$actions[] = '/usr/bin/mysql -uhd0 -phd0 hd0 < ../data/sql/install.sql';
 
 foreach ($actions as $act){
     $output[] = '<b>'.$act.'</b>';
