aff9840ec303a8ddb0782d58af8991f9b9c759ec
Fabio Fazio <fmfazio@gmail.com>
2015-05-14 15:04:21 +0200

    fixed welcome and login to works properly with authenticated/splashoscreen

--- public/assistenza/inc/js/welcome.js
+++ public/assistenza/inc/js/welcome.js
@@ -1,7 +1,28 @@
-$(function(){
+function welcomeLoad()
+{
+	if (splashscreen)
+	{
+	   $( '#welcome' ).modal({});
+	   
+	   $( '#welcome div.modal-xl' ).css( "width", $(window).innerWidth()-50 );
+	   
+	    $('#welcome').on('hidden.bs.modal', function (e) {
+	        if (e) {
+	        	if ( !authenticated ) {
+		            $( '#login' ).modal({
+		                keyboard : false,
+		                backdrop: 'static',
+		            });
+	        	} else {
+					content();
+	            }
+	        }
+	    });
+    }
+	
 	if (authenticated)
 	{
 		var user = JSON.parse($.cookie("user"));
 		$('#name').text(user['name']);
 	}
-});
+}
