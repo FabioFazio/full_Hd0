fdcc37e3e292b3383191829436ec8a38d2cebd1a
Fabio Fazio <fmfazio@gmail.com>
2015-05-22 15:06:29 +0200

    removed move on select in multilistbox... too smart added animation in show hide editor

--- public/assistenza/inc/js/users.js
+++ public/assistenza/inc/js/users.js
@@ -1,9 +1,25 @@
 /* Library for users.inc */
-function usersLoad()
+function usersLoad(e)
 {
-	$( '#usersModal div.modal-xl' ).css( "width", $(window).innerWidth()-50 );
-	
+    //var $related = $(e.relatedTarget);
+    var $current = $(e.currentTarget);
+    
 	$('[data-toggle="confirmation"]').confirmation();
+	
+	// ^ editor animation
+	$current.on('click', '[data-show]', function(){
+		target = $(this).attr('data-show');
+		$(target).fadeIn();
+		window.location = target;
+	});
+	
+	$current.on('click', '[data-hide]', function(){
+		target = $(this).attr('data-hide');
+		$(target).fadeOut();
+	});
+	// $ editor animation
+	
+	
 	
 	//var user = getUser();
 }
